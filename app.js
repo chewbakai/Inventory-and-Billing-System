e57f2a65dc76334964a9d1077c812d3b6caad7c5
chewbakai <kyrahceniza@gmail.com>
2021-06-02 13:30:27 +0800

    packages-added

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-  }
-  
-  const express = require('express')
+const express = require('express')
   const app = express()
   const bcrypt = require('bcrypt')
   const passport = require('passport')
@@ -42,11 +38,7 @@ const e = require('express')
   app.set('view-engine', 'ejs')
   app.use(express.urlencoded({ extended: false }))
   app.use(flash())
-  app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false
-  }))
+  app.use(session({ secret: 'somevalue' }));
   app.use(passport.initialize())
   app.use(passport.session())
   app.use(methodOverride('_method'))
