6fbb46a0ab670b3a9fc425f480ba3c4c73b27006
Francis Tugonon <65629800+Lacheko1998@users.noreply.github.com>
2021-06-07 01:44:23 +0800

    added views

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express')
 const session = require('express-session')
 const uuid = require('uuid')
 const app = express()
-const port = process.env.port || 3000
+const port = process.env.port || 5000
 app.set('view engine', 'ejs')
 
 app.use(express.urlencoded({ extended: true }));
